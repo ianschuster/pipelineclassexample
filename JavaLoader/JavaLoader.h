1f65ec327c97af2aa8c703131b155a36b1f7675f
olekaspt <olekaspt@mail.uc.edu>
2022-03-27 19:23:02 -0400

    Add project files.

---
+++ JavaLoader/JavaLoader.h
@@ -0,0 +1,23 @@
+// The following ifdef block is the standard way of creating macros which make exporting
+// from a DLL simpler. All files within this DLL are compiled with the JAVALOADER_EXPORTS
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see
+// JAVALOADER_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef JAVALOADER_EXPORTS
+#define JAVALOADER_API __declspec(dllexport)
+#else
+#define JAVALOADER_API __declspec(dllimport)
+#endif
+
+#include <string>
+
+
+extern JAVALOADER_API void CreateJVM();
+
+extern JAVALOADER_API void RunJavaProgram1(std::string msg);
+extern JAVALOADER_API void RunJavaProgram2(std::string classFile);
+
+extern JAVALOADER_API void DestroyJVM();
+
+
